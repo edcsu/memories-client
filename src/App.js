d0c09bd461e05b29e4777a9a5c334be658bd1821
Sewannonda Keith Edwin <skeith696@gmail.com>
2022-05-09 02:15:27 +0300

    styles: add app.js syles

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
 
 import memories from "./images/pexels-cottonbro-3419719.jpg"
+
+import useStyles from "./styles";
+
 function App() {
+  const classes = useStyles();
   return (
     <Container maxWidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">Memories</Typography>
-        <img src={memories} alt="memories" height="60" />
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+        <img className={classes.image} src={memories} alt="memories" height="60" />
       </AppBar>
       <Grow in>
         <Container>
